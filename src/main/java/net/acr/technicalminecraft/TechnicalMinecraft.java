8d40546ffa182c78d1d8cdb2204d8b60ba95a245
Ricardo Piedade <98702051+Acr2004@users.noreply.github.com>
2023-10-24 17:34:11 +0100

    Initial commit

---
+++ src/main/java/net/acr/technicalminecraft/TechnicalMinecraft.java
@@ -0,0 +1,37 @@
+package net.acr.technicalminecraft;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(TechnicalMinecraft.MOD_ID)
+public class TechnicalMinecraft {
+    public static final String MOD_ID = "technicalminecraft";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public TechnicalMinecraft() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
